a0fd475bf694a8d25e29c0d9cee33acef40ae24a
unknown <mhd84.ir@gmail.com>
2022-03-25 15:36:34 +0430

    fibonachi

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("ta che adadi az donbale fibonach ra mikhaid");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+int b = 0;
+for (int i = 0; i < number; i++)
+{
+    a = a + b;
+    Console.Write(a);
+    Console.Write(" ");
+    b = a + b;
+    Console.Write(b);
+    Console.Write(" ");
+}
+            
